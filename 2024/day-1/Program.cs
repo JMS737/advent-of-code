ff1dc2fc1b6840f9e6282c7e144e35d6c1708426
Jeremy Sapsed <dev@jsapsed.co.uk>
2025-01-11 13:32:37 +0000

    day 1 / part 2 - dotnet

--- 2024/day-1/Program.cs
+++ 2024/day-1/Program.cs
@@ -1,29 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var inputs = LoadInput("input.txt");
 
-var lines = File.ReadLines("input.txt").ToList();
+var diffScore = GetTotalDifference(inputs);
+Console.WriteLine($"Total difference is: {diffScore}");
 
-List<int> listA = [];
-List<int> listB = [];
+var similarityScore = GetSimilarityScore(inputs);
+Console.WriteLine($"And the similarity score is: {similarityScore}");
 
-foreach (var line in lines)
+(List<int> ListLeft, List<int> ListRight) LoadInput(string filename)
 {
-    Console.WriteLine($"reading line {line}");
-    var values = line.Split("   ");
-    Console.WriteLine($"parsing values {values}");
-    listA.Add(int.Parse(values[0].Trim()));
-    listB.Add(int.Parse(values[1].Trim()));
+
+    var lines = File.ReadLines(filename).ToList();
+
+    List<int> listA = [];
+    List<int> listB = [];
+
+    foreach (var line in lines)
+    {
+        // Console.WriteLine($"reading line {line}");
+        var values = line.Split("   ");
+        listA.Add(int.Parse(values[0].Trim()));
+        listB.Add(int.Parse(values[1].Trim()));
+    }
+
+    return (listA, listB);
 }
 
-listA.Sort();
-listB.Sort();
-
-var sum = 0;
-
-for (int i = 0; i < listA.Count; i++)
+int GetTotalDifference((List<int> Left, List<int> Right) inputs)
 {
-    Console.WriteLine($"adding {listB[i]} minus {listA[i]}");
-    sum += Math.Abs(listB[i] - listA[i]);
+    var listA = inputs.Left.Order().ToList();
+    var listB = inputs.Right.Order().ToList();
+
+    var sum = 0;
+
+    for (int i = 0; i < listA.Count; i++)
+    {
+        // Console.WriteLine($"adding {listB[i]} minus {listA[i]}");
+        sum += Math.Abs(listB[i] - listA[i]);
+    }
+
+    return sum;
 }
 
-Console.WriteLine($"And the sum is: {sum}");
+int GetSimilarityScore((List<int> Left, List<int> Right) inputs)
+{
+    var counts = inputs.Right
+        .GroupBy(i => i)
+        .ToDictionary(i => i.Key, i => i.Count());
+
+    var sum = 0;
+    foreach (var item in inputs.Left)
+    {
+        if (!counts.TryGetValue(item, out var count))
+        {
+            count = 0;
+        }
+
+        // Console.WriteLine($"found {count} occurences of {item}");
+
+        sum += item * count;
+    }
+
+    return sum;
+}
