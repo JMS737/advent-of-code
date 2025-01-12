e63580029de8748f0160caf02aca1d29ca82a606
Jeremy Sapsed <jms737800@gmail.com>
2025-01-12 22:47:22 +0000

    day 1 - C: refactor

--- 2024/day-1/c/main.c
+++ 2024/day-1/c/main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_LOCATIONS 5000
+#define LOAD_ERROR -1
+
+int getlocations(const char *filename, int *left, int *right, const unsigned int size) {
+  FILE *locations = fopen(filename, "r");
+  if (!locations) {
+    fprintf(stderr, "File opening failed!\n");
+    return LOAD_ERROR;
+  }
+
+  int locationLeft, locationRight;
+  int line = 0;
+
+  while (fscanf(locations, "%i   %i\n", &locationLeft, &locationRight) > 0) {
+    if (line == size) {
+      printf("exceeded maximum supported locations of %i\n", size);
+      return LOAD_ERROR;
+    }
+
+    left[line] = locationLeft;
+    right[line] = locationRight;
+    line++;
+  }
+
+  // returns: the number of locations loaded from the file
+  return line;
+}
+
 int compareInt(const void *a, const void *b) {
   int int_a = *((int *)a);
   int int_b = *((int *)b);
@@ -14,43 +42,23 @@ int compareInt(const void *a, const void *b) {
   }
 }
 
-int processLocations(char filename[]) {
-  FILE *locations = fopen(filename, "r");
-  if (!locations) {
-    fprintf(stderr, "File opening failed!\n");
-    return EXIT_FAILURE;
+int processLocations(char *filename) {
+  int left[MAX_LOCATIONS];
+  int right[MAX_LOCATIONS];
+
+  int nloc = getlocations(filename, left, right, MAX_LOCATIONS);
+  if (nloc == LOAD_ERROR) {
+    return LOAD_ERROR;
   }
 
-  int left[1000];
-  int right[1000];
-  int locationLeft;
-  int locationRight;
-  int line = 0;
-  while (fscanf(locations, "%i   %i\n", &locationLeft, &locationRight) > 0) {
-    if (line > 999) {
-      printf("exceeded maximum supported locations of 1000");
-      return EXIT_FAILURE;
-    }
-    left[line] = locationLeft;
-    right[line] = locationRight;
-    line++;
-    //    printf("location found left='%i' right='%i'\n", locationLeft,
-    //    locationRight);
-  }
+  printf("loaded %i locations from file\n", nloc);
 
-  qsort(left, 1000, sizeof(int), compareInt);
-  qsort(right, 1000, sizeof(int), compareInt);
-
-  // example writing to a file
-  FILE *out = fopen("sorted.txt", "w");
-
-  for (int i = 0; i < 1000; i++) {
-    fprintf(out, "%i   %i\n", left[i], right[i]);
-  }
+  qsort(left, nloc, sizeof(int), compareInt);
+  qsort(right, nloc, sizeof(int), compareInt);
 
   int sum = 0;
 
-  for (int i = 0; i < 1000; i++) {
+  for (int i = 0; i < nloc; i++) {
     sum += abs(right[i] - left[i]);
     // printf("%i : L%i R%i Sum %i\n", i, left[i], right[i], sum);
   }
@@ -58,8 +66,8 @@ int processLocations(char filename[]) {
 
   int similarity = 0, iL = 0, iR = 0;
 
-  for (iL = 0; iL < 1000; iL++) {
-    while (iR < 1000 && right[iR] <= left[iL]) {
+  for (iL = 0; iL < nloc; iL++) {
+    while (iR < nloc && right[iR] <= left[iL]) {
 
       // printf("%i:%i : L%i R%i\n", iL, iR, left[iL], right[iR]);
       if (right[iR] == left[iL]) {
@@ -74,9 +82,13 @@ int processLocations(char filename[]) {
   return 0;
 }
 
-int main() {
-  char filename[] = "../input.txt";
-  if (!processLocations(filename)) {
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    printf("Usage:\n    %s <input file name>\n", argv[0]);
+    return 1;
+  }
+
+  if (processLocations(argv[1]) == LOAD_ERROR) {
     return EXIT_FAILURE;
   }
   return 0;
